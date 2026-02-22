314ab1907efd1f2197d348a87a1a00515c1afd00
Mandresy11 <mandresyrakotonanahary@gmail.com>
2026-02-22 23:37:26 +0300

    création modèl user

---
+++ src/app/features/models/user.model.ts
@@ -0,0 +1,30 @@
+export interface User {
+  _id?: string;
+  username: string;
+  fullname: string;
+  gender: 'male' | 'female';
+  email: string;
+  password?: string;
+  isActive?: boolean;
+  role?: 'client' | 'admin';
+  createdAt?: string;
+}
+
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  username: string;
+  fullname: string;
+  gender: 'male' | 'female';
+  email: string;
+  password: string;
+}
+
+export interface AuthResponse {
+  token: string;
+  user: User;
+  message?: string;
+}
